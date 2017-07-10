3479fa02181975fe91a8cf15f16203b333b30f37
jjw.denis <jjwdenis@gmail.com>
2017-07-10 20:19:18 -0300

    Corrigido para eliminar erros de versões com o node

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ var path = require('path');
 exports.megaSena = function(tempDirectory) {
 	let megaSena = require('./lib/mega-sena');
 
-	return new Promise(function(resolve, reject) {
-		megaSena.downloadResultadosMegaSena(path.normalize(tempDirectory))
+	return megaSena.downloadResultadosMegaSena(path.normalize(tempDirectory))
 			.then(function(nomeArquivoComResultados) {
 				return megaSena.htmlToJson(nomeArquivoComResultados);
-			}).then(function (jsonRegistros) {
-				resolve(jsonRegistros);
-			}).catch(function (err) {
-				reject(err);
 			});
-	});
 };
 
 exports.lotoFacil = function(tempDirectory) {
@@ -22,8 +16,6 @@ exports.lotoFacil = function(tempDirectory) {
 	return lotoFacil.downloadResultadosLotoFacil(path.normalize(tempDirectory))
 			.then(function(nomeArquivoComResultados) {
 				return lotoFacil.htmlToJson(nomeArquivoComResultados);
-			}).catch(function (err) {
-				return err;
 			});
 };
 
