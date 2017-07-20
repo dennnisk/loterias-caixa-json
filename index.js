749c62dbdd32a27bbb3ab9e2fa5f822df84e42cf
jjw.denis <jjwdenis@gmail.com>
2017-07-20 20:10:14 -0300

    correcoes de identaçao

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-
 var path = require('path');
 
-exports.megaSena = function(tempDirectory) {
-	let megaSena = require('./lib/mega-sena');
 
-	return megaSena.downloadResultadosMegaSena(path.normalize(tempDirectory))
-			.then(function(nomeArquivoComResultados) {
-				return megaSena.htmlToJson(nomeArquivoComResultados);
-			});
+
+exports.megaSena = function(tempDirectory) {
+  let megaSena = require('./lib/mega-sena');
+
+  return megaSena.downloadResultadosMegaSena(path.normalize(tempDirectory))
+    .then(function(nomeArquivoComResultados) {
+      return megaSena.htmlToJson(nomeArquivoComResultados);
+    });
 };
+
+
 
 exports.lotoFacil = function(tempDirectory) {
-	let lotoFacil = require('./lib/loto-facil');
+  let lotoFacil = require('./lib/loto-facil');
 
-	return lotoFacil.downloadResultadosLotoFacil(path.normalize(tempDirectory))
-			.then(function(nomeArquivoComResultados) {
-				return lotoFacil.htmlToJson(nomeArquivoComResultados);
-			});
+  return lotoFacil.downloadResultadosLotoFacil(path.normalize(tempDirectory))
+    .then(function(nomeArquivoComResultados) {
+      return lotoFacil.htmlToJson(nomeArquivoComResultados);
+    });
 };
-
